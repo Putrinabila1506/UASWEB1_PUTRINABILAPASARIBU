6386c062f3447ebac2b5a0a4228c895b36c7d04e
Putrinabila1506 <watia13peach@gmail.com>
2026-01-16 22:16:15 +0700

    koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "db_penjualann2";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
